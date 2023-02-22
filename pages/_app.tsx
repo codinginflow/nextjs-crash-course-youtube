92e48cc029a56b32808b30689624c691af5f0fb5
Coding in Flow <info@codinginflow.com>
2023-02-22 10:26:16 -0800

    End of part 3

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/globals.css';
+import { Inter } from '@next/font/google';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Container } from 'react-bootstrap';
+import styles from "@/styles/App.module.css";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <div className={inter.className}>
+      <Head>
+        <title>NextJS News App</title>
+        <meta name="description" content="NextJS crash course by Coding in Flow" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container className={styles.pageContainer}>
+        <Component {...pageProps} />
+      </Container>
+    </div>
+  );
+}
---
+++ pages/search.tsx
@@ -0,0 +1,9 @@
+const SearchNewsPage = () => {
+    return ( 
+        <main>
+            <h1>Search News</h1>
+        </main>
+     );
+}
+ 
+export default SearchNewsPage;
