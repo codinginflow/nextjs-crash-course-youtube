2cec924d9e4679a8127004c2da276116f04bcc87
Coding in Flow <info@codinginflow.com>
2023-02-23 01:11:22 -0800

    End of part 9

--- components/NewsArticleEntry.tsx
+++ components/NewsArticleEntry.tsx
@@ -1,5 +1,8 @@
 import { NewsArticle } from "@/models/NewsArticles";
+import Image from "next/image";
 import { Card } from "react-bootstrap";
+import placeholderImage from "@/assets/images/newsarticle_placeholder.jpg";
+import styles from "@/styles/NewsArticleEntry.module.css";
 
 interface NewsArticleEntryProps {
     article: NewsArticle,
@@ -12,9 +15,12 @@ const NewsArticleEntry = ({ article: { title, description, url, urlToImage } }: 
     return (
         <a href={url}>
             <Card className="h-100">
-                <Card.Img
-                    variant="top"
-                    src={validImageUrl}
+                <Image
+                    src={validImageUrl || placeholderImage}
+                    width={500}
+                    height={200}
+                    alt="News article image"
+                    className={`card-img-top ${styles.image}`}
                 />
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
